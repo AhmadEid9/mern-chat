44cb35d2383ea99fb56d512ad6d7921308969355
AhmadEid9 <ahmad.eid.2.4.99@gmail.com>
2024-10-25 00:08:16 +0300

    <Client> Initializing Login and Signup Scafhold

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,11 @@
-import Login from "./pages/Login"
+import Signup from "./pages/Signup"
 
 function App() {
 
   return (
     <>
       <div className="p-4 h-screen flex items-center justify-center">
-        <Login />
+        <Signup />
       </div>
     </>
   )
